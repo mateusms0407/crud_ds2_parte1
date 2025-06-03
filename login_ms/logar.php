8c115b6da02a3408d4630d28f4e1810378b671d5
mateusms0407 <168552718+mateusms0407@users.noreply.github.com>
2025-06-03 12:37:48 -0300

    atualizaçao nome variavel

--- login_ms/logar.php
+++ login_ms/logar.php
@@ -4,8 +4,8 @@ if(isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['senha']) &
     require "usuario.php";
     $ms = new usuario();
 
-    $email = addslashes($_POST['email']);
-    $senha = addslashes($_POST['senha']);
+    $email = trim($_POST['email']);
+    $senha = trim($_POST['senha']);
 
     if($ms -> login($email,$senha) == true){
         if(isset($_SESSION['idUser'])){
